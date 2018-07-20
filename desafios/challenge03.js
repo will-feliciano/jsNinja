9ff0ec48c01162689c55c65d1505952b73d2ca1c
will <william@youtrendz.com>
2018-07-20 17:37:25 -0300

    Challenge04

--- desafios/challenge03.js
+++ desafios/challenge03.js
@@ -120,7 +120,7 @@ Quantos anos a `pessoa` tem agora? (Use a instrução para responder e
 comentários inline ao lado da instrução para mostrar qual foi a resposta
 retornada)
 */
-//31
+//Olá! eu tenho 31 anos!
 
 /*
 Agora, faça a `pessoa` caminhar alguns metros, invocando o método `andar` 3x,
---
+++ desafios/challenge04.js
@@ -0,0 +1,181 @@
+/*
+Declare uma variável chamada `isTruthy`, e atribua a ela uma função que recebe
+um único parâmetro como argumento. Essa função deve retornar `true` se o
+equivalente booleano para o valor passado no argumento for `true`, ou `false`
+para o contrário.
+*/
+var isTruthy = function(algo){
+    return algo == true ? true : false ;
+}
+
+// Invoque a função criada acima, passando todos os tipos de valores `falsy`.
+isTruthy(0);
+//false
+isTruthy(-0);
+//false
+isTruthy('');
+//false
+isTruthy(false);
+//false
+isTruthy(undefined);
+//false
+
+/*
+Invoque a função criada acima passando como parâmetro 10 valores `truthy`.
+*/
+isTruthy(true);
+//true
+isTruthy('1');
+//true
+isTruthy(1);
+//true
+isTruthy('text');
+//true
+isTruthy(2+2);
+//true
+isTruthy(2345);
+//true
+isTruthy("Will");
+//true
+isTruthy(9);
+//true
+isTruthy(9-1);
+//true
+isTruthy(-11);
+//true
+
+/*
+Declare uma variável chamada `carro`, atribuindo à ela um objeto com as
+seguintes propriedades (os valores devem ser do tipo mostrado abaixo):
+- `marca` - String
+- `modelo` - String
+- `placa` - String
+- `ano` - Number
+- `cor` - String
+- `quantasPortas` - Number
+- `assentos` - Number - cinco por padrão
+- `quantidadePessoas` - Number - zero por padrão
+*/
+var carro = { marca: 'Fiat', modelo: 'Uno', placa: 'CSI0101', ano: 2010, cor: 'Vermelho', quantasPortas: 5, assentos: 5, qtdePessoas: 0 }
+
+/*
+Crie um método chamado `mudarCor` que mude a cor do carro conforme a cor
+passado por parâmetro.
+*/
+carro.mudaCor = function(color){
+    return this.cor = color;
+}
+
+/*
+Crie um método chamado `obterCor`, que retorne a cor do carro.
+*/
+carro.obterCor = function(){
+    return this.cor;
+}
+
+/*
+Crie um método chamado `obterModelo` que retorne o modelo do carro.
+*/
+carro.obterModelo = function(){
+    return this.modelo;
+}
+
+/*
+Crie um método chamado `obterMarca` que retorne a marca do carro.
+*/
+carro.obterMarca = function(){
+    return this.marca;
+}
+
+/*
+Crie um método chamado `obterMarcaModelo`, que retorne:
+"Esse carro é um [MARCA] [MODELO]"
+Para retornar os valores de marca e modelo, utilize os métodos criados.
+*/
+carro.obterMarcaModelo = function(){
+    return 'Esse carro é um '+ this.marca +' '+this.modelo;
+}
+
+/*
+Crie um método que irá adicionar pessoas no carro. Esse método terá as
+seguintes características:
+- Ele deverá receber por parâmetro o número de pessoas entrarão no carro. Esse
+número não precisa encher o carro, você poderá acrescentar as pessoas aos
+poucos.
+- O método deve retornar a frase: "Já temos [X] pessoas no carro!"
+- Se o carro já estiver cheio, com todos os assentos já preenchidos, o método
+deve retornar a frase: "O carro já está lotado!"
+- Se ainda houverem lugares no carro, mas a quantidade de pessoas passadas por
+parâmetro for ultrapassar o limite de assentos do carro, então você deve
+mostrar quantos assentos ainda podem ser ocupados, com a frase:
+"Só cabem mais [QUANTIDADE_DE_PESSOAS_QUE_CABEM] pessoas!"
+- Se couber somente mais uma pessoa, mostrar a palavra "pessoa" no retorno
+citado acima, no lugar de "pessoas".
+*/
+carro.addPessoas = function(num){
+    if((this.qtdePessoas+num)<=5){
+        this.qtdePessoas += num;
+        return "Já temos "+ this.qtdePessoas +" pessoas no carro!";
+    }
+    else{
+        if(this.qtdePessoas < 5){
+            return "Só cabem mais "+ (5-this.qtdePessoas) +" pessoas!";
+        }
+        else{
+            return "O carro já está lotado!";
+        }
+    }
+}
+
+/*
+Agora vamos verificar algumas informações do carro. Para as respostas abaixo,
+utilize sempre o formato de invocação do método (ou chamada da propriedade),
+adicionando comentários _inline_ ao lado com o valor retornado, se o método
+retornar algum valor.
+
+Qual a cor atual do carro?
+*/
+carro.obterCor();
+//preto
+
+// Mude a cor do carro para vermelho.
+carro.mudaCor('vermelho');
+
+// E agora, qual a cor do carro?
+carro.obterCor();
+//vermelho
+
+// Mude a cor do carro para verde musgo.
+carro.mudaCor('verde musgo');
+
+// E agora, qual a cor do carro?
+carro.obterCor();
+//verde musgo
+
+// Qual a marca e modelo do carro?
+carro.obterMarcaModelo();
+//Esse carro é um Fiat Uno
+
+// Adicione 2 pessoas no carro.
+carro.addPessoas(2);
+//Já temos 2 pessoas no carro!
+
+// Adicione mais 4 pessoas no carro.
+carro.addPessoas(4);
+//Só cabem mais 3 pessoas!
+
+// Faça o carro encher.
+carro.addPessoas(3);
+//Já temos 5 pessoas no carro!
+
+// Tire 4 pessoas do carro.
+carro.addPessoas(-4);
+//Já temos 1 pessoas no carro!
+
+// Adicione 10 pessoas no carro.
+carro.addPessoas(10);
+//Só cabem mais 4 pessoas!
+
+// Quantas pessoas temos no carro?
+carro.qtdePessoas;
+//1
